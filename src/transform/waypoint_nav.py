aa55ebf2dc28e007797d65da1acef9eae282fe9e
ssliang <43868758+ssliang@users.noreply.github.com>
2021-03-05 00:54:02 -0500

    Update waypoint_nav.py
    Added 1. euclidean_dist (helper fxn for navigate_to_waypoint_x()), 2. get_steering (skeleton for steering angle calc), 3. navigate_to_waypoint_x() (completed for waypoint selection & half completed for navigation)

--- src/transform/waypoint_nav.py
+++ src/transform/waypoint_nav.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python 
 
 # Preparation of waypoints as PoseArray(). 
-# Test by: 
+# Test waypoints path: 
 # rospy.init_node('testing')
 # waypoints = get_waypoints('saved_waypoints.csv')
 # waypoint_poses = prepare_waypoint_poses(waypoints)
 # rospy.loginfo("Published {} waypoints.".format(len(waypoint_poses.poses))) 
+# Test waypoint navigation:
+# rospy.init_node('testing')
+# waypoints = get_waypoints('saved_waypoints.csv')
+# waypoint_poses = prepare_waypoint_poses(waypoints)
 
+navigate_to_waypoint_x(waypoint_poses.poses[1].pose.position, 0, waypoint_poses)
 import pandas as pd
 import numpy as np
 import math 
@@ -92,3 +97,79 @@ def prepare_waypoint_poses(waypoints):
         path.poses.append( pose )
 
     return path
+
+def euclidean_dist(x1, x2, y1, y2):
+    """
+    Calculates Euclidean distance.
+    -------------
+    Params: xy coordinates
+    -------------
+    Returns: distance 
+    """
+    return np.sqrt(np.square(x1 - x2) + np.square(y1 - y2)) 
+
+def get_steering(curr_pos, waypoint, curr_heading):
+    """
+    Calculates steering angle to point 
+    towards next waypoint
+    -------------
+    Params: 
+    curr_pos: current vehicle position in x, y 
+    waypoint: upcoming waypoint pose()
+    curr_heading: current vehicle heading in (degrees/radians?)
+    -------------
+    Returns:
+    Steering angle 
+    """
+    # angle between waypoint and current position in radians 
+    desired_heading = math.atan((waypoint.pose.position.y - curr_pos.pose.position.y)/(waypoint.pose.position.x - curr_pos.pose.position.x))
+
+    if curr_heading != desired_heading:
+        #rotate steering_angle degrees
+        print("placeholder")
+
+def navigate_to_waypoint_x(curr_pos, curr_heading, path):
+    """
+    Handles waypoint selection and waypoint navigation
+    -------------
+    Params:
+    curr_pos: current vehicle position as point object
+    curr_heading: current vehicle heading in (degrees/radians?)
+    path: waypoints as Path()
+    """
+    amt_waypoints = len(path.poses)
+    endpt = path.poses[-1]
+
+    # waypt radius buffer in ?? units
+    # choose CEP? but make sure waypoints are far away enough from 
+    # each other to be greather than CEP
+    buffer_radius = 2
+
+    # maximum rotation angle
+    max_rotation = 0    
+    
+    for i in range(amt_waypoints):
+        print(i)
+        curr_waypt = path.poses[i]
+        # Calculate the distance between the current waypoint and endpoint. 
+        dist_waypt_endpt = euclidean_dist(endpt.pose.position.x, curr_waypt.pose.position.x, endpt.pose.position.y, curr_waypt.pose.position.y)
+        # Calculate the distance between the current waypoint and current vehicle position. 
+        dist_waypt_pt = euclidean_dist(endpt.pose.position.x, curr_pos.x, endpt.pose.position.y, curr_pos.y)
+
+        print(dist_waypt_endpt, dist_waypt_pt)
+        # continue towards curr_waypt
+        if dist_waypt_endpt < dist_waypt_pt:
+            print(" heading towards curr waypoint ")
+            # send steering angle
+
+            # send throttle value
+
+            # continuously check if current_pos is within buffer radius
+            # should be getting new curr_pos updates here
+            dist_waypt_pt = euclidean_dist(endpt.pose.position.x, curr_pos.x, endpt.pose.position.y, curr_pos.y)
+
+            if dist_waypt_pt < buffer_radius:
+                print("within buffer radius, head towards next one")
+                continue
+            else:
+                print("Curr_waypoint already reached, heading to next one")
